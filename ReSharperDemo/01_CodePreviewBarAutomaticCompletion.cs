ee7c8c1cea4a0b33ec90aee570b08a1713a101b8
Hugin Antony <HuginAntony@gmail.com>
2019-07-24 15:57:25 +0200

    Adding EPPlus nuget package to ReSharperDemos.Tests project

--- ReSharperDemo/01_CodePreviewBarAutomaticCompletion.cs
+++ ReSharperDemo/01_CodePreviewBarAutomaticCompletion.cs
@@ -11,8 +11,8 @@ public class Flights
         emirates.GetLocation();
         emirates.CalculateReward();
 
-        //Ctrl+Space x2 will find the Controller reference from the CoreMvcWebsite project and import it
-        //Controller
+        //Ctrl+Space x2 will find the Controller reference from the ReSharperDemos.Tests project and import it
+        //ExcelPackage
 
         //Invoke quick info by pressing Ctrl+Q on the class and the method name
         var mango = new Mango();
