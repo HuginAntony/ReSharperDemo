3b708d12dc4683df767fe2e590a4a9a1cd5e9823
Hugin Antony <HuginAntony@gmail.com>
2019-07-25 09:47:36 +0200

    Adding JavaScript quick fix Demo

--- ReSharperDemo/09_Search.js
+++ ReSharperDemo/09_Search.js
@@ -1,58 +1,36 @@
-﻿  function UpdateValidations() {
-        initialValue = $("#" + "<%= InitialNarrative.ClientID %>").val();
-        var cbo = $find("<%=cboVariablePrefix.ClientID %>");
-        var narMax = $get('<%= txtFreeNarrative.ClientID %>').maxLength;
-        if (cbo) {
-            var item = cbo.get_selectedItem();
-            if (item) {
-                var itemText = item.get_text();
-                var txtMax = $get("hdnPrefixMax");
-                var maxlength;
-                if (itemText.length > narMax) {
-                    if (initialValue == "Prefix") {
-                        maxLength = 0;
-                        var variableValidator = $get('<%= variableValidator.ClientID %>');
-                        ValidatorEnable(variableValidator, false);
-                        $('#LabelNarrativeWarning').css("visibility", "visible");
-                        Disable();
-                    }
-                } else {
-                    if (initialValue == "Prefix") {
-                        var variableValidator = $get('<%= variableValidator.ClientID %>');
-                        ValidatorEnable(variableValidator, true);
-                        maxlength = txtMax.value - itemText.length;
-                        $('#LabelNarrativeWarning').css("visibility", "hidden");
-                        Enable();
-                    }
-                }
-            }
-        }
-        var txt = $find("<%= txtVariablePrefix.ClientID %>");
-        if (txt) {
-            txt.set_maxLength(maxlength);
-            txt.set_value("");
-        }
+﻿function UpdateValidations() {
+    var name = "Lisa";
+
+    if (name = "Lisa") {
+        alert('Simpson');
     }
 
-    function Disable() {
-        var objState = $find("<%= txtVariablePrefix.ClientID %>");
-        objState.disable();
+    var txt = $find("<%= txtVariablePrefix.ClientID %>");
+    if (txt) {
+        txt.set_maxLength(maxlength);
+        txt.set_value("");
     }
+}
 
-    function Enable() {
-        var objState = $find("<%= txtVariablePrefix.ClientID %>");
-        objState.enable();
-    }
+function Disable() {
+    var objState = $find("<%= txtVariablePrefix.ClientID %>");
+    objState.disable();
+}
 
-    function UpdateNarratives() {
+function Enable() {
+    var objState = $find("<%= txtVariablePrefix.ClientID %>");
+    objState.enable();
+}
 
-        $("#<%= prefix.ClientID %>").show();
-        $("#<%= @fixed.ClientID %>").show();
-        $("#<%= free.ClientID %>").show();
-        $("#<%= autogen.ClientID %>").show();
-        $("#<%= template.ClientID %>").show();
-    }
+function UpdateNarratives() {
 
-    function showMessage() {
+    $("#<%= prefix.ClientID %>").show();
+    $("#<%= @fixed.ClientID %>").show();
+    $("#<%= free.ClientID %>").show();
+    $("#<%= autogen.ClientID %>").show();
+    $("#<%= template.ClientID %>").show();
+}
+
+function showMessage() {
     alert('Testing');
 }
